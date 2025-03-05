7db43e3ec56232269231e35afcebc42fbfb09ff6
michelesponsale@gmail.com <16162947+LudWittg@users.noreply.github.com>
2025-03-05 05:04:17 +0100

    style: fix indentation

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,23 +6,23 @@ self.addEventListener('install', function(event) {
     self.hadPreviousSW = !!self.registration.active;
     // Skip waiting so this service worker becomes active immediately
     self.skipWaiting();
-  });
+});
   
-  self.addEventListener('activate', function(event) {
-    event.waitUntil(
-      self.clients.claim()
-        // Unregister all service workers including itself
-        .then(() => self.registration.unregister())
-        .then(() => {
-          // Only reload clients if there was a previous service worker
-          if (self.hadPreviousSW) {
-            return self.clients.matchAll();
-          } else {
-            return []; // Skip reload
-          }
-        })
-        .then(clients => {
-          clients.forEach(client => client.navigate(client.url));
-        })
-    );
-  });
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    self.clients.claim()
+      // Unregister all service workers including itself
+      .then(() => self.registration.unregister())
+      .then(() => {
+        // Only reload clients if there was a previous service worker
+        if (self.hadPreviousSW) {
+          return self.clients.matchAll();
+        } else {
+          return []; // Skip reload
+        }
+      })
+      .then(clients => {
+        clients.forEach(client => client.navigate(client.url));
+      })
+  );
+});
