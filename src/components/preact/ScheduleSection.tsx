120784ab6ba538eab59d8038a99f94dd52005e8d
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2025-03-27 00:16:19 +0100

    added filters and talk level info

--- src/components/preact/ScheduleSection.tsx
+++ src/components/preact/ScheduleSection.tsx
@@ -1,27 +1,27 @@
-import { TALKS, TALKS_TIME_BLOCKS, ROOMS } from '@/lib/sessionize'
+// import { TALKS, TALKS_TIME_BLOCKS, ROOMS } from '@/lib/sessionize'
+import type { Talk } from '@/lib/sessionize'
 import clsx from 'clsx'
-import { useState, type Dispatch, type StateUpdater } from 'preact/hooks'
+import { useMemo, useState, type Dispatch, type StateUpdater } from 'preact/hooks'
+import {
+    MaterialSymbolsBookmarkAddOutlineRounded,
+    MaterialSymbolsBookmarkCheckOutlineRounded,
+    MaterialSymbolsFilterAltOutline,
+} from './icons'
 
-export function MaterialSymbolsBookmarkAddOutlineRounded(props: any) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
-            <path
-                fill="currentColor"
-                d="m12 18l-4.2 1.8q-1 .425-1.9-.162T5 17.975V5q0-.825.588-1.412T7 3h5q.425 0 .713.288T13 4t-.288.713T12 5H7v12.95l5-2.15l5 2.15V12q0-.425.288-.712T18 11t.713.288T19 12v5.975q0 1.075-.9 1.663t-1.9.162zm0-13H7h6zm5 2h-1q-.425 0-.712-.288T15 6t.288-.712T16 5h1V4q0-.425.288-.712T18 3t.713.288T19 4v1h1q.425 0 .713.288T21 6t-.288.713T20 7h-1v1q0 .425-.288.713T18 9t-.712-.288T17 8z"
-            ></path>
-        </svg>
-    )
-}
-
-export function MaterialSymbolsBookmarkCheckOutlineRounded(props: any) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
-            <path
-                fill="currentColor"
-                d="m10.95 11.175l-.725-.725q-.3-.275-.7-.275t-.7.3t-.3.7t.3.7l1.4 1.425q.3.3.713.3t.712-.3l3.525-3.55q.3-.3.3-.7t-.3-.7t-.7-.3t-.7.3zM12 18l-4.2 1.8q-1 .425-1.9-.162T5 17.975V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v12.975q0 1.075-.9 1.663t-1.9.162zm0-2.2l5 2.15V5H7v12.95zM12 5H7h10z"
-            ></path>
-        </svg>
-    )
+export function getTalkTimeBlocks(talks: Talk[]) {
+    return Object.entries(
+        talks.reduce<Record<string, Talk[]>>((acc, talk) => {
+            if (!talk.startTime || !talk.duration) {
+                return acc
+            }
+            const startTime = talk.startTime.toISOString()
+            if (!acc[startTime]) {
+                acc[startTime] = []
+            }
+            acc[startTime].push(talk)
+            return acc
+        }, {}),
+    ).sort(([a], [b]) => a.localeCompare(b))
 }
 
 const startOffset = new Date('2025-04-12').setHours(8, 30)
@@ -29,6 +29,8 @@ const startOffset = new Date('2025-04-12').setHours(8, 30)
 function minutes(d: Date) {
     return d.getHours() * 60 + d.getMinutes()
 }
+
+const ROOMS = ['Aula magna', 'IWD', 'seconda aula talk', 'terza aula talks']
 
 const EXTRA_EVENTS = [
     {
@@ -60,22 +62,6 @@ const EXTRA_EVENTS = [
     },
 ]
 
-const TIME_LABELS = [
-    ...TALKS_TIME_BLOCKS.map(([blockStart, talks], i) => ({
-        startTime: new Date(blockStart),
-        duration: Math.min(
-            talks.reduce((acc, talk) => (acc > 0 ? Math.min(acc, talk.duration!) : talk.duration!), 0),
-            TALKS_TIME_BLOCKS[i + 1]
-                ? minutes(new Date(TALKS_TIME_BLOCKS[i + 1][0])) - minutes(new Date(blockStart))
-                : Infinity,
-        ),
-    })),
-    ...EXTRA_EVENTS.map(event => ({
-        startTime: new Date(event.startTime),
-        duration: event.duration,
-    })),
-].sort((a, b) => a.startTime.getTime() - b.startTime.getTime())
-
 export function useLocalStorageState<T>(key: string, initialValue: T): [T, Dispatch<StateUpdater<T>>] {
     const [storedValue, setStoredValue] = useState<T>(() => {
         try {
@@ -106,12 +92,80 @@ export function useLocalStorageState<T>(key: string, initialValue: T): [T, Dispa
     return [storedValue, setValue]
 }
 
-export const ScheduleSection = ({}) => {
+type ScheduleSectionProps = {
+    talks: Talk[]
+}
+
+export const ScheduleSection = ({ talks }: ScheduleSectionProps) => {
+    const categories = useMemo(() => [...new Set(talks.map(talk => talk.category))], [talks])
+
+    const levels = useMemo(() => [...new Set(talks.map(talk => talk.level))], [talks])
+
+    const talkTimeBlocks = useMemo(() => getTalkTimeBlocks(talks), [talks])
+
+    const TIME_LABELS = [
+        ...talkTimeBlocks.map(([blockStart, talks], i) => ({
+            startTime: new Date(blockStart),
+            duration: Math.min(
+                talks.reduce((acc, talk) => (acc > 0 ? Math.min(acc, talk.duration!) : talk.duration!), 0),
+                talkTimeBlocks[i + 1]
+                    ? minutes(new Date(talkTimeBlocks[i + 1][0])) - minutes(new Date(blockStart))
+                    : Infinity,
+            ),
+        })),
+        ...EXTRA_EVENTS.map(event => ({
+            startTime: new Date(event.startTime),
+            duration: event.duration,
+        })),
+    ].sort((a, b) => a.startTime.getTime() - b.startTime.getTime())
+
     const [selectedTalks, setSelectedTalks] = useLocalStorageState<string[]>('devfest-2025-user-schedule-v1', [])
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+    const [selectedLevel, setSelectedLevel] = useState<string | null>(null)
 
     return (
         <section id="schedule" class="wide">
             <h1>Schedule</h1>
+
+            <div class="schedule-filters">
+                <div class="title">
+                    <MaterialSymbolsFilterAltOutline width={'1em'} height={'1em'} />
+                    Filters
+                </div>
+                <div class="subtitle">Categories</div>
+                <div class="filter-buttons">
+                    {categories.map(category => (
+                        <button
+                            class={clsx('chip', {
+                                selected: selectedCategory === category,
+                                hidden: selectedCategory && selectedCategory !== category,
+                            })}
+                            onClick={() => {
+                                setSelectedCategory(selectedCategory === category ? null : category)
+                            }}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
+                <div class="subtitle">Levels</div>
+                <div class="filter-buttons">
+                    {levels.map(level => (
+                        <button
+                            class={clsx('chip', {
+                                selected: selectedLevel === level,
+                                hidden: selectedLevel && selectedLevel !== level,
+                            })}
+                            onClick={() => {
+                                setSelectedLevel(selectedLevel === level ? null : level)
+                            }}
+                        >
+                            {level}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
             <div
                 class="talk-schedule"
                 style={{
@@ -120,7 +174,7 @@ export const ScheduleSection = ({}) => {
             >
                 {TIME_LABELS.map(time => (
                     <div
-                        class="schedule-cell header"
+                        class="schedule-cell header-row"
                         style={{
                             ['--start-time']: `${minutes(time.startTime)}`,
                             ['--duration']: `${time.duration}`,
@@ -137,60 +191,78 @@ export const ScheduleSection = ({}) => {
                         </div>
                     </div>
                 ))}
-                {TALKS.filter(talk => !!talk.room).map(talk => (
-                    <a
-                        class={clsx('schedule-cell', 'interactive', {
-                            selected: selectedTalks.includes(talk.id),
-                        })}
+
+                {ROOMS.map(room => (
+                    <div
+                        class="schedule-cell header-column"
                         style={{
-                            ['--start-time']: `${minutes(new Date(talk.startTime!))}`,
-                            ['--duration']: `${talk.duration!}`,
-                            ['--room']: `${ROOMS.indexOf(talk.room!)}`,
+                            ['--room']: `${ROOMS.indexOf(room)}`,
                         }}
-                        href={`/talk/${talk.id}`}
                     >
-                        <div class="title">{talk.title}</div>
-
-                        <div class="metadata">
-                            <div class="chip">{talk.category}</div>
-                        </div>
-
-                        <div class="actions">
-                            <button
-                                class="flat square rounded"
-                                onClick={e => {
-                                    e.preventDefault()
-
-                                    setSelectedTalks(
-                                        selectedTalks.includes(talk.id)
-                                            ? selectedTalks.filter(id => id !== talk.id)
-                                            : [...selectedTalks, talk.id],
-                                    )
-                                }}
-                            >
-                                {selectedTalks.includes(talk.id) ? (
-                                    <MaterialSymbolsBookmarkCheckOutlineRounded />
-                                ) : (
-                                    <MaterialSymbolsBookmarkAddOutlineRounded />
-                                )}
-                            </button>
-                        </div>
-
-                        <div class="speakers">
-                            {talk.speakers.map(speaker => (
-                                <>
-                                    <div class="text">
-                                        {speaker.firstName} {speaker.lastName}
-                                    </div>
-                                    <img
-                                        src={speaker.profilePicture}
-                                        alt={`${speaker.firstName} ${speaker.lastName}`}
-                                    />
-                                </>
-                            ))}
-                        </div>
-                    </a>
+                        <div class="text">{room}</div>
+                    </div>
                 ))}
+
+                {talks
+                    .filter(talk => !!talk.room)
+                    .map(talk => (
+                        <a
+                            class={clsx('schedule-cell', 'interactive', {
+                                selected: selectedTalks.includes(talk.id),
+                                hidden:
+                                    (selectedCategory && talk.category !== selectedCategory) ||
+                                    (selectedLevel && talk.level !== selectedLevel),
+                            })}
+                            style={{
+                                ['--start-time']: `${minutes(new Date(talk.startTime!))}`,
+                                ['--duration']: `${talk.duration!}`,
+                                ['--room']: `${ROOMS.indexOf(talk.room!)}`,
+                            }}
+                            href={`/talk/${talk.id}`}
+                        >
+                            <div class="title">{talk.title}</div>
+
+                            <div class="metadata">
+                                <div class="chip">{talk.category}</div>
+                                <div class="chip">{talk.level}</div>
+                            </div>
+
+                            <div class="actions">
+                                <button
+                                    class="flat square rounded"
+                                    onClick={e => {
+                                        e.preventDefault()
+
+                                        setSelectedTalks(
+                                            selectedTalks.includes(talk.id)
+                                                ? selectedTalks.filter(id => id !== talk.id)
+                                                : [...selectedTalks, talk.id],
+                                        )
+                                    }}
+                                >
+                                    {selectedTalks.includes(talk.id) ? (
+                                        <MaterialSymbolsBookmarkCheckOutlineRounded />
+                                    ) : (
+                                        <MaterialSymbolsBookmarkAddOutlineRounded />
+                                    )}
+                                </button>
+                            </div>
+
+                            <div class="speakers">
+                                {talk.speakers.map(speaker => (
+                                    <>
+                                        <div class="text">
+                                            {speaker.firstName} {speaker.lastName}
+                                        </div>
+                                        <img
+                                            src={speaker.profilePicture}
+                                            alt={`${speaker.firstName} ${speaker.lastName}`}
+                                        />
+                                    </>
+                                ))}
+                            </div>
+                        </a>
+                    ))}
 
                 {EXTRA_EVENTS.map(event => (
                     <div
---
+++ src/components/preact/icons.tsx
@@ -0,0 +1,32 @@
+export function MaterialSymbolsBookmarkAddOutlineRounded(props: any) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="m12 18l-4.2 1.8q-1 .425-1.9-.162T5 17.975V5q0-.825.588-1.412T7 3h5q.425 0 .713.288T13 4t-.288.713T12 5H7v12.95l5-2.15l5 2.15V12q0-.425.288-.712T18 11t.713.288T19 12v5.975q0 1.075-.9 1.663t-1.9.162zm0-13H7h6zm5 2h-1q-.425 0-.712-.288T15 6t.288-.712T16 5h1V4q0-.425.288-.712T18 3t.713.288T19 4v1h1q.425 0 .713.288T21 6t-.288.713T20 7h-1v1q0 .425-.288.713T18 9t-.712-.288T17 8z"
+            ></path>
+        </svg>
+    )
+}
+
+export function MaterialSymbolsBookmarkCheckOutlineRounded(props: any) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="m10.95 11.175l-.725-.725q-.3-.275-.7-.275t-.7.3t-.3.7t.3.7l1.4 1.425q.3.3.713.3t.712-.3l3.525-3.55q.3-.3.3-.7t-.3-.7t-.7-.3t-.7.3zM12 18l-4.2 1.8q-1 .425-1.9-.162T5 17.975V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v12.975q0 1.075-.9 1.663t-1.9.162zm0-2.2l5 2.15V5H7v12.95zM12 5H7h10z"
+            ></path>
+        </svg>
+    )
+}
+
+export function MaterialSymbolsFilterAltOutline(props: any) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" {...props}>
+            <path
+                fill="currentColor"
+                d="M11 20q-.425 0-.712-.288T10 19v-6L4.2 5.6q-.375-.5-.112-1.05T5 4h14q.65 0 .913.55T19.8 5.6L14 13v6q0 .425-.288.713T13 20zm1-7.7L16.95 6h-9.9zm0 0"
+            ></path>
+        </svg>
+    )
+}
