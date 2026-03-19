6e001bb12cead6fbfca02c5ee493b9b53a86d890
michelesponsale@gmail.com <16162947+LudWittg@users.noreply.github.com>
2026-03-19 18:59:00 +0100

    chore: update sponsor data

--- src/assets/data-sponsors/index.ts
+++ src/assets/data-sponsors/index.ts
@@ -7,11 +7,13 @@ import logobjectLogo from './logobject.svg'
 import nextworksLogo from './nextworks.svg'
 import nttDataLogo from './ntt_data.svg'
 import randstadLogo from './randstad.svg'
+import rossoLogo from './rosso.svg'
 import seacomLogo from './seacom.svg'
 import tdGroupLogo from './td_group.svg'
 import vianovaLogo from './vianova.svg'
 import woltersKluwerLogo from './wolters_kluwer.svg'
 import wondersysLogo from './wondersys.svg'
+import zerynthLogo from './zerynth.svg'
 
 export const sponsors = {
     main: [
@@ -21,12 +23,22 @@ export const sponsors = {
             url: 'https://www.google.it/',
         },
     ],
-    // diamond: [
-    //     {
-    //         name: 'Nextworks srl',
-    //         logo: nextworksLogo,
-    //         url: 'https://www.nextworks.it/en/',
-    //     },
+     diamond: [
+        {
+            name: 'Nextworks srl',
+            logo: nextworksLogo,
+            url: 'https://www.nextworks.it/en/',
+        },
+        {
+            name: 'Zerynth S.p.A',
+            logo: zerynthLogo,
+            url: 'https://zerynth.com/',
+        },
+        {
+            name: 'Ema Health SRL',
+            logo: rossoLogo,
+            url: 'https://donarosso.it/',
+        },
     //     {
     //         name: 'Vianova spa',
     //         logo: vianovaLogo,
@@ -37,16 +49,16 @@ export const sponsors = {
     //         logo: nttDataLogo,
     //         url: 'https://it.nttdata.com/',
     //     },
-    //     {
-    //         name: 'Garmin Italy Technologies ',
-    //         logo: garminLogo,
-    //         url: 'https://www.garmin.com',
-    //     },
-    //     {
-    //         name: 'TD GROUP ITALIA SRL',
-    //         logo: tdGroupLogo,
-    //         url: 'https://www.tdgroupitalia.it/',
-    //     },
+        {
+            name: 'Garmin Italy Technologies',
+            logo: garminLogo,
+            url: 'https://www.garmin.com',
+        },
+        {
+            name: 'TD GROUP ITALIA SRL',
+            logo: tdGroupLogo,
+            url: 'https://www.tdgroupitalia.it/',
+        },
     //     {
     //         name: 'CCH Tagetik | Wolters Kluwer',
     //         logo: woltersKluwerLogo,
@@ -72,7 +84,7 @@ export const sponsors = {
     //         logo: randstadLogo,
     //         url: 'https://www.randstad.it/digital/',
     //     },
-    // ],
+    ],
     // platinum: [
     //     {
     //         name: 'Ascom',
@@ -80,16 +92,16 @@ export const sponsors = {
     //         url: 'https://www.ascom.com/it/',
     //     },
     // ],
-    // silver: [
-    //     {
-    //         name: 'Wondersys srl',
-    //         logo: wondersysLogo,
-    //         url: 'https://www.wonder-sys.com/',
-    //     },
-    //     {
-    //         name: 'Seacom',
-    //         logo: seacomLogo,
-    //         url: 'https://www.seacom.it/',
-    //     },
-    // ],
+    silver: [
+        {
+            name: 'Wondersys srl',
+            logo: wondersysLogo,
+            url: 'https://www.wonder-sys.com/',
+        },
+        // {
+        //     name: 'Seacom',
+        //     logo: seacomLogo,
+        //     url: 'https://www.seacom.it/',
+        // },
+    ],
 }
